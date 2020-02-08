400ba36ef71013df051c654dc87d8c7957f5d495
Memo Chou <memochou1993@gmail.com>
2020-02-09 03:11:32 +0800

    Update database connection timeout

--- database/main.go
+++ database/main.go
@@ -19,7 +19,7 @@ var (
 )
 
 func init() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	opts := options.Client().ApplyURI(uri)
